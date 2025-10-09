401ff3505d265102f960ef4235c816a59dc131a7
hubghost709-dev <hubghost709@gmail.com>
2025-10-08 23:03:23 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,25 +3,27 @@ import { authMiddleware } from '@clerk/nextjs';
 import createMiddleware from 'next-intl/middleware';
 
 const intlMiddleware = createMiddleware({
-  locales: ['en', 'es'],
-  defaultLocale: 'es'
+  locales: ['en', 'es'],
+  defaultLocale: 'es'
 });
 
 export default authMiddleware({
-  beforeAuth: (req) => {
-    return intlMiddleware(req);
-  },
-  publicRoutes: [
-    '/',
-    '/:locale',
-    '/:locale/sign-in',
-    '/:locale/sign-up'
-  ]
+  // **LÍNEA COMENTADA:** Con esto, Clerk se ejecutará sin la lógica de localización
+  // beforeAuth: (req) => {
+  //   return intlMiddleware(req);
+  // },
+  
+  publicRoutes: [
+    '/',
+    '/:locale',
+    '/:locale/sign-in',
+    '/:locale/sign-up'
+  ]
 });
 
 export const config = {
-  matcher: [
-    '/((?!_next|.*\\..*|favicon.ico).*)',
-    '/(en|es)/:path*'
-  ]
+  matcher: [
+    '/((?!_next|.*\\..*|favicon.ico).*)',
+    '/(en|es)/:path*'
+  ]
 };
