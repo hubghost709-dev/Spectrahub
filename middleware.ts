a175abb871b0b9407d08358feb044a25b3e333b5
hubghost709-dev <hubghost709@gmail.com>
2025-10-09 20:02:36 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,30 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
-import createMiddleware from 'next-intl/middleware';
-import { authMiddleware } from '@clerk/nextjs';
+import createMiddleware from "next-intl/middleware";
 
-const intlMiddleware = createMiddleware({
-  locales: ['en', 'es'],
-  defaultLocale: 'es',
+export default createMiddleware({
+  locales: ["en", "es"],
+  defaultLocale: "es",
 });
-
-const handler = authMiddleware({
-  beforeAuth: (req) => {
-    return intlMiddleware(req);
-  },
-  publicRoutes: [
-    '/',
-    '/:locale',
-    '/:locale/sign-in',
-    '/:locale/sign-up',
-  ],
-});
-
-export default handler;
 
 export const config = {
   matcher: [
-    '/((?!_next|.*\\..*|favicon.ico).*)',
-    '/(en|es)/:path*',
+    "/((?!_next|.*\\..*|favicon.ico).*)",
+    "/(en|es)/:path*",
   ],
-  runtime: 'nodejs', // 👈 Fuerza ejecución en entorno Node.js (no Edge)
 };
