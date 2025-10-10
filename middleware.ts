a5fec54a7eaf4501e61371f208dc6c1b1a198c5f
hubghost709-dev <hubghost709@gmail.com>
2025-10-09 21:24:46 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,30 @@
-import createMiddleware from "next-intl/middleware";
+import { authMiddleware } from '@clerk/nextjs';
+import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
+import { NextResponse, type NextRequest } from 'next/server';
 
-export default createMiddleware({
-  locales: ["en", "es"],
-  defaultLocale: "es",
+const intlMiddleware = createMiddleware(routing);
+
+export default authMiddleware({
+  publicRoutes: [
+    "/", "/api/webhooks(.*)", "/api/uploadthing",
+    "/:username", "/search", "/:locale/sign-in", "/:locale/sign-up"
+  ],
+  beforeAuth: (req: NextRequest) => {
+    const path = req.nextUrl.pathname;
+    
+    if (path.startsWith('/api') || path.includes('.') || path.startsWith('/_next')) {
+      return NextResponse.next();
+    }
+
+    return intlMiddleware(req);
+  },
 });
 
 export const config = {
   matcher: [
-    "/((?!_next|.*\\..*|favicon.ico).*)",
-    "/(en|es)/:path*",
+    '/((?!_next/static|_next/image|favicon.ico).*)', 
+    '/(en|es)/:path*'
   ],
+  runtime: 'nodejs', // 🔑 importante para AWS
 };
