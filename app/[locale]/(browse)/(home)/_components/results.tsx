8ecc7482596fcdc5448e3a1b618b8e6b6c5b7788
hubghost709-dev <hubghost709@gmail.com>
2026-02-11 18:05:02 -0400

    Update results.tsx

--- app/[locale]/(browse)/(home)/_components/results.tsx
+++ app/[locale]/(browse)/(home)/_components/results.tsx
@@ -43,17 +43,18 @@ export default async function Results() {
       )}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
-        {verifiedStreams.map((result) => (
-          <ResultCard 
-            key={result.id} 
-            data={{
-              user: result.user,
-              isLive: result.isLive,
-              name: result.name,
-              thumbnailUrl: result.thumbnailUrl
-            }} 
-          />
-        ))}
+    {verifiedStreams.map((result) => (
+  <ResultCard 
+    key={result.id} 
+    data={{
+      user: result.user,
+      isLive: result.isLive,
+      name: result.name,
+      // ✅ Si está en vivo, usa thumbnailUrl (captura), si no usa offlineThumbnailUrl
+      thumbnailUrl: result.isLive ? result.thumbnailUrl : result.offlineThumbnailUrl
+    }} 
+  />
+))}
       </div>
     </div>
   );
