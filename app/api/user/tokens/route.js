239235811b6433a78cd6e63fa0e74f51fde3c047
hubghost709-dev <hubghost709@gmail.com>
2026-02-14 12:10:17 -0400

    Update route.js

--- app/api/user/tokens/route.js
+++ app/api/user/tokens/route.js
@@ -2,6 +2,8 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";  // Cambiado a 'next/server'
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req) {
   try {
     const { userId } = auth();
